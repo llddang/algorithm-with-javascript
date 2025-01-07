bf65c4b11732ad77511413b4d64aa71599f2a6a9
llddang <77055208+llddang@users.noreply.github.com>
2025-01-07 12:30:41 +0900

    Create 002.JadenCase 문자열 만들기.js

---
+++ Programmers/Level 2/002.JadenCase 문자열 만들기.js
@@ -0,0 +1,6 @@
+function solution(s) {
+    return s.split(" ")
+            .map((str) => 
+                 str.charAt(0).toUpperCase() + str.substring(1).toLowerCase())
+            .join(" ");
+}
