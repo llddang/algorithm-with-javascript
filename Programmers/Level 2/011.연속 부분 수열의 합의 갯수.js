e4bc39c145e9b910947313585258dd8be8c53c3d
llddang <77055208+llddang@users.noreply.github.com>
2025-01-07 16:06:10 +0900

    Update 011.연속 부분 수열의 합의 갯수.js

--- Programmers/Level 2/011.연속 부분 수열의 합의 갯수.js
+++ Programmers/Level 2/011.연속 부분 수열의 합의 갯수.js
@@ -1,10 +1,10 @@
 function solution(arr) {
     let answer = new Set(), sum, len = arr.length;
     
-    for(let size=1; size<=len; size++){
-        for(let i=0; i<len; i++){
-            sum = 0;
-            for(let k=0; k<size; k++) sum += arr[(i + k) % len];
+    for(let st=0; st<len; st++){
+        sum = 0;
+        for(let en=0; en<len; en++){
+            sum += arr[(st + en) % len];
             answer.add(sum);
         }
     }
