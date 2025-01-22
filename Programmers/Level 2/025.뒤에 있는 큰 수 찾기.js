0ef45b6cedf4934ecedea756727924dcaaa0036c
llddang <77055208+llddang@users.noreply.github.com>
2025-01-22 09:55:06 +0900

    Create 025.뒤에 있는 큰 수 찾기.js

---
+++ Programmers/Level 2/025.뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(nums) {
+    const dp = new Array(nums.length).fill(-1);
+    const stack = [0];
+    for (let i = 1; i < nums.length; i++) {
+        while (stack.length && nums[stack[stack.length - 1]] < nums[i])
+            dp[stack.pop()] = nums[i];
+        stack.push(i);
+    }
+    return dp;
+}
