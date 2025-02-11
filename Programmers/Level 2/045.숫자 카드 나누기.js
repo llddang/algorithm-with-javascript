a2ae9e9638984b4709a7125b2d30da5084ba24e5
llddang <77055208+llddang@users.noreply.github.com>
2025-02-11 20:29:26 +0900

    Create 045.숫자 카드 나누기.js

---
+++ Programmers/Level 2/045.숫자 카드 나누기.js
@@ -0,0 +1,37 @@
+function gcd(a, b) {
+    if (a < b) [a, b] = [b, a];
+    while (b !== 0) [a, b] = [b, a % b];
+    return a;
+}
+
+function getDivisors(num) {
+    const divisors = new Set();
+    
+    const sqrt = Math.sqrt(num);
+    for (let i = 1; i <= sqrt; i++) {
+        if (num % i === 0) {
+            divisors.add(i);
+            divisors.add(num / i);
+        }
+    }
+    return Array.from(divisors).sort((a, b) => b - a);
+}
+
+function check(gcdNum, array) {
+    const divisors = getDivisors(gcdNum);
+    for(let s of divisors) {
+        const res = array.every((n) => Math.floor(n / s) !== n / s);
+        if(res) return s;
+    }
+    return 0;
+}
+
+function solution(arrayA, arrayB) {
+    const gcdA = arrayA.reduce((acc, num) => gcd(num, acc));
+    const gcdB = arrayB.reduce((acc, num) => gcd(num, acc));
+    
+    const ansA = check(gcdA, arrayB);
+    const ansB = check(gcdB, arrayA);
+    
+    return Math.max(ansA, ansB);
+}
